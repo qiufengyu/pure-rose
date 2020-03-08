e6191a6bd60919939699e7ef804177eb5332c603
Fengyu <mrqiufengyu@gmail.com>
2020-03-09 02:34:05 +0800

    bug fix

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -10,7 +10,7 @@ import { Product } from './product';
   providedIn: 'root',
 })
 export class ProductService {
-  private productsUrl = 'api/produdcts';
+  private productsUrl = 'api/products';
 
   constructor(private http: HttpClient) { }
 
